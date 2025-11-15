dc504af1bc21a14b067917e7ccbf29dd2b0b7a9e
ivantang26 <124768677+IvanTang26@users.noreply.github.com>
2025-11-15 19:53:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ const UUID = process.env.UUID || '0f805275-3da0-41ba-a0e0-1af4bd5cb5ed'; // 使�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || '';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'snownight.snowynight.ggff.net';          // 固定隧道域名,留空即启用临时隧道
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiYmE3MTNhYTk4Mjg0MGY3NjE3YmU3ZWI3NTIwNDc3NzMiLCJ0IjoiZTM2NGQ1MTAtYjczMy00OTBhLThiZjgtNWQwYThkMTYxNGIxIiwicyI6Ik1tUXdOek5pT0RrdE1ESTVZeTAwWlRka0xUZ3hPVFF0WVRBNU9HRmlOVFppTkRRMiJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
-const NAME = process.env.NAME || '';                        // 节点名称
+const NAME = process.env.NAME || 'snownight';                        // 节点名称
 
 // 创建运行文件夹
 if (!fs.existsSync(FILE_PATH)) {
